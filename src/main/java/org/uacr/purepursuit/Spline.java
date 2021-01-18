54ad802e21b7b55d914688c15ac8f5263c5dadf1
sidbhushan <sidhbhushan@gmail.com>
2021-01-18 01:27:33 -0700

    Better spline algorithm

--- src/main/java/org/uacr/purepursuit/Spline.java
+++ src/main/java/org/uacr/purepursuit/Spline.java
@@ -6,26 +6,102 @@ public class Spline {
 
     private double[] x;
     private double[] y;
-    private double[] dy;
+
+    public Polynomial[] curves;
 
     public Spline(double[] x, double[] y) {
-        this.x = x;
-        this.y = y;
-        dy = getNaturalDerivatives(x, y);
-    }
-
-    public double eval(double xVal) {
-        int i = 1;
-        while (x[i] < xVal) {
-            i++;
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("Must provide same number of x and y values.");
         }
 
-        double t = (xVal - x[i - 1]) / (x[i] - x[i - 1]);
+        this.x = x;
+        this.y = y;
 
-        double a = dy[i - 1] * (x[i] - x[i - 1]) - (y[i] - y[i - 1]);
-        double b = -dy[i] * (x[i] - x[i - 1]) + (y[i] - y[i - 1]);
+        this.curves = generatePolynomials(this.x, this.y);
+    }
 
-        return (1 - t) * y[i - 1] + t * y[i] + t * (1 - t) * (a * (1 - t) + b * t);
+    public double eval(double input) {
+        for (Polynomial curve : curves) {
+            if (input >= curve.getLowerBound() && input <= curve.getUpperBound()) {
+                return curve.eval(input);
+            }
+        }
+
+        throw new IllegalArgumentException("Input not in domain of spline");
+    }
+
+    public double[] eval(double[] inputs) {
+        double[] results = new double[inputs.length];
+        for (int i = 0; i < inputs.length; i++) {
+            results[i] = eval(inputs[i]);
+        }
+        return results;
+    }
+
+    private static Polynomial[] generatePolynomials(double[] x, double[] y) {
+        final int n = x.length;
+        int currentRow = 0;
+
+        double[][] equationsMatrix = getZeros(4 * n - 4, 4 * n - 3);
+
+        // Boundary conditions
+        // c_0 = 0
+        equationsMatrix[currentRow++][2] = 1;
+        // 0 = 2 c_n-2 + 6 d_n-2 (x_n-1 - x_n-2)
+        equationsMatrix[currentRow][4 * (n - 2) + 2] = 2;
+        equationsMatrix[currentRow++][4 * (n - 2) + 3] = 6 * (x[n - 1] - x[n - 2]);
+        for (int i = 0; i < n - 1; i++) {
+            // y_i = a_i
+            equationsMatrix[currentRow][4 * i] = 1;
+            equationsMatrix[currentRow++][4 * n - 4] = y[i];
+            // y_i+1 = a_i + b_i (x_i+1 - x_i) + c_i (x_i+1 - x_i)^2 + d_i (x_i+1 - x_i)^3
+            equationsMatrix[currentRow][4 * i] = 1;
+            equationsMatrix[currentRow][4 * i + 1] = x[i + 1] - x[i];
+            equationsMatrix[currentRow][4 * i + 2] = Math.pow(x[i + 1] - x[i], 2);
+            equationsMatrix[currentRow][4 * i + 3] = Math.pow(x[i + 1] - x[i], 3);
+            equationsMatrix[currentRow++][4 * n - 4] = y[i+1];
+        }
+        for (int i = 0; i < n - 2; i++) {
+            // 0 = b_i + 2c_i (x_i+1 - x_i) + 3d_i (x_i+1 - x_i)^2 - b_i+1
+            equationsMatrix[currentRow][4 * i + 1] = 1;
+            equationsMatrix[currentRow][4 * i + 2] = 2 * (x[i + 1] - x[i]);
+            equationsMatrix[currentRow][4 * i + 3] = 3 * Math.pow(x[i + 1] - x[i], 2);
+            equationsMatrix[currentRow++][4 * (i + 1) + 1] = -1;
+            // 0 = 2c_i + 6d_i (x_i+1 - x_i) - 2c_i+1
+            equationsMatrix[currentRow][4 * i + 2] = 2;
+            equationsMatrix[currentRow][4 * i + 3] = 6 * (x[i + 1] - x[i]);
+            equationsMatrix[currentRow++][4 * (i + 1) + 2] = -2;
+        }
+
+        Polynomial[] result = new Polynomial[n - 1];
+        double[] coefficients = solveMatrix(equationsMatrix);
+        for (int i = 0; i < n - 1; i++) {
+            result[i] = new Polynomial(3, x[i], x[i + 1], x[i],
+                    coefficients[4 * i], coefficients[4 * i + 1],
+                    coefficients[4 * i + 2], coefficients[4 * i + 3]);
+        }
+
+        return result;
+    }
+
+    private static double[][] getZeros(int rows, int columns) {
+        double[][] result = new double[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                result[i][j] = 0;
+            }
+        }
+
+        return result;
+    }
+
+    private static void printArray(double[][] array) {
+        for (double[] doubles : array) {
+            for (double aDouble : doubles) {
+                System.out.print(aDouble + " ");
+            }
+            System.out.println();
+        }
     }
 
     private static double[] solveMatrix(double[][] matrix) {
@@ -71,46 +147,60 @@ public class Spline {
         matrix[row2] = temp;
     }
 
-    private double[] getNaturalDerivatives(double[] x, double[] y) {
-        double[][] matrix = new double[x.length][x.length + 1];
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < x.length + 1; j++) {
-                matrix[i][j] = 0;
+    private static class Polynomial {
+        private int order;
+        private double[] coefficients;
+
+        private double lowerBound;
+        private double upperBound;
+        private double offset;
+
+        public Polynomial(int order, double lowerBound, double upperBound, double offset, double... coefficients) {
+            if (coefficients.length != order + 1) {
+                throw new IllegalArgumentException("Polynomial must have the same number of coefficients as its order");
             }
+            this.order = order;
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+            this.offset = offset;
+            this.coefficients = coefficients;
         }
 
-        for (int i = 1; i < x.length - 1; i++) {
-            matrix[i][i - 1] = 1 / (x[i] - x[i - 1]);
-            matrix[i][i] = 2 * (1 / (x[i] - x[i - 1]) + 1 / (x[i + 1] - x[i]));
-            matrix[i][i + 1] = 1 / (x[i + 1] - x[i]);
-            matrix[i][x.length] = 3 * ((y[i] - y[i - 1]) / ((x[i] - x[i - 1]) * (x[i] - x[i - 1])) + (y[i + 1] - y[i]) / ((x[i + 1] - x[i]) * (x[i + 1] - x[i])));
+        public double getLowerBound() {
+            return lowerBound;
         }
 
-        matrix[0][0] = 2 / (x[1] - x[0]);
-        matrix[0][1] = 1 / (x[1] - x[0]);
-        matrix[0][x.length] = 3 * (y[1] - y[0]) / ((x[1] - x[0]) * (x[1] - x[0]));
+        public double getUpperBound() {
+            return upperBound;
+        }
 
-        matrix[x.length - 1][x.length - 2] = 1 / (x[x.length - 1] - x[x.length - 2]);
-        matrix[x.length - 1][x.length - 1] = 2 / (x[x.length - 1] - x[x.length - 2]);
-        matrix[x.length - 1][x.length] = 3 * (y[x.length - 1] - y[x.length - 2]) / ((x[x.length - 1] - x[x.length - 2]) * (x[x.length - 1] - x[x.length - 2]));
+        public double eval(double x) {
+            double result = 0;
+            for (int i = 0; i < coefficients.length; i++) {
+                result += coefficients[i] * Math.pow(x - offset, i);
+            }
+            return result;
+        }
 
-        return solveMatrix(matrix);
+        public String toString() {
+            String result = "Polynomial of order " + order + ": ";
+            for (int i = 0; i < coefficients.length; i ++) {
+                result += coefficients[i] + "(x-" + offset + ")^" + i + "+ ";
+            }
+            return result + "\b\b";
+        }
     }
 
     public static void main(String[] args) {
-        double[] x = {0, 1, 2, 3, 4, 5};
-        double[] y = {0, 2, 4, 6, 4, 0};
+        double[] x = new double[]{0, 1, 2, 3, 4, 5};
+        double[] y = new double[]{0, 2, 4, 6, 4, 0};
         Spline s = new Spline(x, y);
-        double[] res = new double[500];
-        for (int i = 0; i < res.length; i ++) {
-            res[i] = s.eval((((double) i) / 100));
+        double[] testX = new double[500];
+        for (int i = 0; i < 500; i ++) {
+            testX[i] = ((double) i) / 100;
         }
-        System.out.println(Arrays.toString(res));
-//        System.out.print("[");
-//        for (int i = 0; i < res.length; i ++) {
-//            double xVal = ((double) i) / 100;
-//            System.out.print("(" + xVal + ", " + res[i] + "), ");
-//        }
-//        System.out.println("\b\b]");
+        System.out.println(Arrays.toString(testX));
+        System.out.println(Arrays.toString(s.eval(testX)));
+        System.out.println(Arrays.toString(s.curves));
     }
 }
